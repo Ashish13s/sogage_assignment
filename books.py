44bfed10024d281bfdcc3fa9f85415f3ad455040
Ashish Singh <ashishsingh2024.13s@gmail.com>
2023-04-14 03:39:47 +0530

    feat: Initialized repository

--- books.py
+++ books.py
@@ -1,6 +1,5 @@
 import json
 import os
-from typing import Any, Dict, List
 
 import requests
 from dotenv import load_dotenv
@@ -8,10 +7,10 @@ from dotenv import load_dotenv
 
 class GoogleBooksAPI:
     def __init__(self):
-        self.base_url: str = "https://www.googleapis.com/books/v1/volumes"
+        self.base_url = "https://www.googleapis.com/books/v1/volumes"
 
-    def get_params(self, search_query: str) -> Dict[str, str]:
-        params: Dict[str, str] = {
+    def get_params(self, search_query):
+        params = {
             "q": search_query,
             "printType": "books",
             "projection": "lite",
@@ -20,10 +19,10 @@ class GoogleBooksAPI:
         }
         return params
 
-    def get_response(self, search_query: str) -> Dict[str, Any]:
-        params: Dict[str, str] = self.get_params(search_query)
+    def get_response(self, search_query):
+        params = self.get_params(search_query)
         try:
-            response: requests.models.Response = requests.get(
+            response = requests.get(
                 self.base_url, params=params
             )
             return response.json()
@@ -31,20 +30,20 @@ class GoogleBooksAPI:
             print(e)
         return {}
 
-    def get_books(self, search_query: str) -> List[Dict[str, Any]]:
-        response_json: Dict[str, Any] = self.get_response(search_query)
-        all_books: List[Dict[str, Any]] = []
+    def get_books(self, search_query):
+        response_json = self.get_response(search_query)
+        all_books = []
         if "items" in response_json:
-            books: List[Dict[str, Any]] = response_json["items"]
+            books = response_json["items"]
             for book in books:
-                book_info: Dict[str, Any] = book["volumeInfo"]
+                book_info = book["volumeInfo"]
                 all_books.append(book_info)
         return all_books
 
-    def format_for_json(self, books) -> List[Dict[str, str]]:
-        formatted_books: List[Dict[str, str]] = []
+    def format_for_json(self, books):
+        formatted_books = []
         for book in books:
-            formatted_book: Dict[str, str] = {}
+            formatted_book = {}
             formatted_book["Title"] = book.get("title", "")
             formatted_book["Content"] = book.get("description", "")
             formatted_books.append(formatted_book)
@@ -55,8 +54,8 @@ class WikiBooksAPI:
     def __init__(self) -> None:
         self.base_url = "https://en.wikibooks.org/w/api.php"
 
-    def get_params(self, search_query: str) -> Dict[str, str]:
-        params: Dict[str, str] = {
+    def get_params(self, search_query):
+        params = {
             "action": "query",
             "format": "json",
             "list": "search",
@@ -65,10 +64,10 @@ class WikiBooksAPI:
         }
         return params
 
-    def get_response(self, search_query: str) -> Dict[str, Any]:
-        params: Dict[str, str] = self.get_params(search_query)
+    def get_response(self, search_query):
+        params = self.get_params(search_query)
         try:
-            response: requests.models.Response = requests.get(
+            response = requests.get(
                 self.base_url, params=params
             )
             return response.json()
@@ -76,7 +75,7 @@ class WikiBooksAPI:
             print(e)
         return {}
 
-    def get_books(self, search_query: str) -> List[Dict[str, Any]]:
+    def get_books(self, search_query):
         response_json = self.get_response(search_query)
         all_books = []
         if "search" in response_json["query"]:
@@ -86,14 +85,14 @@ class WikiBooksAPI:
                 all_books.append(formatted_book)
         return all_books
 
-    def format_response(self, response) -> Dict[str, Any]:
+    def format_response(self, response):
         response["snippet"] = response["snippet"].replace(
             '<span class="searchmatch">', ""
         )
         response["snippet"] = response["snippet"].replace("</span>", "")
         return response
 
-    def format_for_json(self, books) -> List[Dict[str, str]]:
+    def format_for_json(self, books):
         formatted_books = []
         for book in books:
             formatted_book = {}
@@ -103,13 +102,13 @@ class WikiBooksAPI:
         return formatted_books
 
 
-def print_books(all_books: List[Dict[str, Any]]) -> None:
+def print_books(all_books) -> None:
     for book in all_books:
         print(json.dumps(book, indent=2))
 
 
 def write_to_json(
-    google_books: List[Dict[str, str]], wiki_books: List[Dict[str, str]]
+    google_books, wiki_books
 ) -> None:
     file_name = "output.json"
     with open(file_name, "w") as f:
@@ -119,11 +118,11 @@ def write_to_json(
 
 if __name__ == "__main__":
     load_dotenv()
-    google_books: GoogleBooksAPI = GoogleBooksAPI()
-    wiki_books: WikiBooksAPI = WikiBooksAPI()
-    search_query: str = input("Enter search string: ")
-    g_books: List[Dict[str, Any]] = google_books.get_books(search_query)
-    w_books: List[Dict[str, Any]] = wiki_books.get_books(search_query)
+    google_books = GoogleBooksAPI()
+    wiki_books = WikiBooksAPI()
+    search_query = input("Enter search string: ")
+    g_books = google_books.get_books(search_query)
+    w_books = wiki_books.get_books(search_query)
     print("**************************************************")
     print("Google Books API\n\n")
     print_books(g_books)
